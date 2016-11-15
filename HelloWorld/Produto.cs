3a2c44329eb10efb718573bf91e631bc928c31c2
leonardosouza32 <leonardosouza32@gmail.com>
2016-11-14 23:22:55 -0200

    mais outro teste

---
+++ HelloWorld/Produto.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HelloWorld
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+        public Produto(string nome, decimal preco)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+        }
+    }
+}
---
+++ HelloWorldTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld;
+
+namespace HelloWorldTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Produto p = new Produto("Sabao", 10.0m);
+            Assert.IsTrue(p.Nome == "Sabao");
+            Assert.IsTrue(p.Preco == 10.0m);
+        }
+    }
+}
